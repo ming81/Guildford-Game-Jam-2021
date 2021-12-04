89499d77726fa9a0bc95353c41f33776612612bd
Merlin <77647205+Merlin6019@users.noreply.github.com>
2021-12-04 15:20:11 +0000

    Level Design + Character Controller Tweaks

---
+++ Assets/Invector FootStep System/Scripts/vFootstepMenuComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Invector.Utils;
+
+namespace Invector.vCharacterController
+{
+    public partial class vMenuComponent
+    {
+        [MenuItem("Invector/FootStep System/Add FootStep Component")]
+        static void FootStepMenu()
+        {
+            if (Selection.activeGameObject)
+                Selection.activeGameObject.AddComponent<vFootStep>();
+            else
+                Debug.Log("Please select a GameObject to add the component.");
+        }
+
+        [MenuItem("Invector/FootStep System/Create New AudioSurface")]
+        static void NewAudioSurface()
+        {
+            vScriptableObjectUtility.CreateAsset<vAudioSurface>();
+        }       
+    }
+}
